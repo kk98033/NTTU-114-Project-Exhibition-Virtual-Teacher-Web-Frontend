70403bc7727930f9ea479236753d4891795500c4
Chia Yu Yang <a8829037@gmail.com>
2024-10-07 04:55:57 +0800

    modify: app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 // src/App.js
 import React from 'react';
 import VRMViewer from './components/VRMViewer';
+import ChatBox from './components/ChatBox';
+import BottomBar from './components/BottomBar';
+import QuickQuestions from './components/QuickQuestions';
 
 function App() {
     return (
         <div className="App">
             <h1>VRM Viewer</h1>
             <VRMViewer />
+            <ChatBox />
+            <QuickQuestions />
+            <BottomBar />
         </div>
     );
 }
